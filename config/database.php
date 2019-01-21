570ac629d733b7866a88aeb431fc8c4876eba788
irerimwenda <bmugendi98@gmail.com>
2019-01-21 12:31:02 +0300

    Migration basics

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel_test'),
+            'database' => env('DB_DATABASE', 'laravel_blog'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'password'),
             'unix_socket' => env('DB_SOCKET', ''),
