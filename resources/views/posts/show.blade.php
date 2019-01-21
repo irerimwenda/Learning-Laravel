3326b5ab41c60ccbe5b555ab4bac9af9fdde8798
irerimwenda <bmugendi98@gmail.com>
2019-01-21 17:55:41 +0300

    Displaying from DB with timestamp :fire:

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,8 +6,48 @@
 
 @section('content')
 
+<div class="row">
+
+<div class="col-md-8">
+
 <h1>{{ $post->title }}</h1>
 <p class="lead">{{ $post->body }}</p>
+
+</div>
+
+<div class="col-md-4">
+    <div class="well">
+
+    <dl class="dl-horizantal">
+        <dt>Created At</dt>
+        <dd>{{date('M j, Y h:i a', strtotime($post->created_at))}}</dd>
+    </dl>
+
+    <dl class="dl-horizantal">
+            <dt>Updated At</dt>
+            <dd>{{date('M j, Y h:i a', strtotime($post->updated_at))}}</dd>
+        </dl>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-6">
+                    <a href="" class="btn btn-info">Edit</a>
+            </div>
+
+            <div class="col-sm-6">
+                    <a href="" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+       
+
+</div>
+</div>
+
+</div>
+
+
+
 @endsection()
 
 @include('partials._javascript')
